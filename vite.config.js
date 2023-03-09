a553da24480b6f9b5a71d11ab5b678b1457dbf1d
Zamir Narro <znarro@bsale.com.pe>
2023-03-08 21:28:13 -0500

    Add Svelte preprocess and aliases

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
+import sveltePreprocess from "svelte-preprocess";
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(),
+      onwarn: (warning, handler) => {
+        const { code } = warning;
+        // Omite los warning por clases no utilizadas (sucede mucho con Material)
+        if (code === "css-unused-selector") return;
+        handler(warning);
+      },
+    }),
+  ],
   root,
   appType: "mpa",
   build: {
@@ -17,6 +28,12 @@ export default defineConfig({
         login: resolve(root, "layouts/login.html"),
         products: resolve(root, "layouts/products.html"),
       },
+    },
+  },
+  resolve: {
+    alias: {
+      $lib: resolve("src/lib"),
+      $locales: resolve("config/locales"),
     },
   },
 });
