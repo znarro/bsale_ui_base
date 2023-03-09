a920861d4b2a6dfa2c0ad7c4b740adba98ae34a5
Zamir Narro <znarro@bsale.com.pe>
2023-03-08 21:29:19 -0500

    Modify layouts

--- src/pages/login/main.js
+++ src/pages/login/main.js
@@ -1,7 +1,8 @@
+import "../../styles/main.scss";
 import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById("app"),
+  target: document.getElementById("content"),
 });
 
 export default app;
